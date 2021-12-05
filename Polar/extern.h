9f094d8001fa543dba26d217ef71da7e14112b79
allenalvarezloya <59852918+allenalvarezloya@users.noreply.github.com>
2021-12-04 19:29:37 -0500

    Delete Polar directory

--- Polar/extern.h
+++
@@ -1,5 +0,0 @@
-extern "C" { 
-	void matm1(double *,double *,double *,double *,double *,double *, double *, double *, double *, double *,double *,double *); 
-	void matm2(double *,double *,double *,double *,double *,double *, double *, double *, double *, double *,double *,double *); 
-	void matm3(double *,double *,double *,double *,double *,double *, double *, double *, double *, double *,double *,double *); 
-}
